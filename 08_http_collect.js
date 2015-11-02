b56ecab9cfffe7d1a9d89b59b3531d8d62d6d7c3
Ajwah <johnson786.kevin+git@gmail.com>
2015-11-02 12:41:07 +0000

    implement 08 with a library that manages the collection of the entire response

--- 08_http_collect.js
+++ 08_http_collect.js
@@ -1,13 +1,29 @@
+var bl = require('bl')
+
 var http = require('http');
 var url = process.argv[2];
-http.get(url, function callback (response) {
-  response.setEncoding('utf8');
-  var body = '';
-  response.on("data", function (data) {
-    body += data;
+
+function solution1() {
+  http.get(url, function callback (response) {
+    response.setEncoding('utf8');
+    var body = '';
+    response.on("data", function (data) {
+      body += data;
+    });
+    response.on('end', function(){
+      console.log(body.length);
+      console.log(body);
+    });
   });
-  response.on('end', function(){
-    console.log(body.length);
-    console.log(body);
+}
+
+function solution2() {
+  http.get(url, function callback (response) {
+    response.pipe(bl(function (err, data) {
+      console.log(data.toString().length);
+      console.log(data.toString());
+    }));
   });
-});
+}
+
+solution2();
